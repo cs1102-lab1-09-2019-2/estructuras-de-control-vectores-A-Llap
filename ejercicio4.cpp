318c9ad50cd4854c8dceac65f66d062d1e1dd496
A-Llap <54276084+A-Llap@users.noreply.github.com>
2019-08-28 22:29:42 -0500

    ej4 completo
    .

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -14,7 +14,15 @@ Escribir un programa que imprima lo siguiente:
 using std::cout;
 
 int main() {
-
+  for (int i=3; i<9;i++){
+    for (int j=1;j<i;j++){
+      cout << j;
+    }
+    for (int j=1;j<(9-i);j++){
+      cout << "*";
+    }
+    cout << "\n";
+  }
 
     return 0;
-}
+  }
