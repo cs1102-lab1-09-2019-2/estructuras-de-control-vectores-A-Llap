fedf1f17bd73b1bc3db73c98fb3690fca31c7f1b
A-Llap <54276084+A-Llap@users.noreply.github.com>
2019-08-28 22:46:48 -0500

    ej6 completo
    .

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -4,3 +4,24 @@
  Y encuentra el mayor valor: 30
 
  */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c, mayor;
+    cin >> a>>b>>c;
+    if (a<b){
+      mayor = b;
+    }else{
+      mayor = a;
+    }
+    if (mayor < c){
+      mayor = c;
+    cout << "Mayor: " << mayor;
+    }else{
+      cout << "Mayor: " << mayor;
+    }
+
+return 0;
+}
